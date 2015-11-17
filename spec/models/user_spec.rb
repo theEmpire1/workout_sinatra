1e6f4bd7594fcddad857d8911ef836916e61c480
Drew <cobra.kai@manheim.com>
2015-11-16 20:54:14 -0500

    Added basic user model validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,14 @@ describe User do
     joe = FactoryGirl.create(:user)
     expect(joe).to be_valid
   end
+
+  it 'does not pass validation unless name is present' do
+    not_joe = FactoryGirl.build(:user, name: '    ')
+    expect(not_joe).not_to be_valid
+  end
+
+  it 'does not pass validation unless email is present' do
+    no_email_joe = FactoryGirl.build(:user, email: '    ')
+    expect(no_email_joe).not_to be_valid
+  end
 end
