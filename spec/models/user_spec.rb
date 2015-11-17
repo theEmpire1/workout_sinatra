7234461b794e51e1cc878bf7a7d45812bc43cb62
Drew <cobra.kai@manheim.com>
2015-11-16 21:00:26 -0500

    User model attrs length validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,4 +15,16 @@ describe User do
     no_email_joe = FactoryGirl.build(:user, email: '    ')
     expect(no_email_joe).not_to be_valid
   end
+
+  it 'restricts user.name to 50 characters' do
+    long_name = 'joe' + ('e' * 48)
+    long_joe = FactoryGirl.build(:user, name: long_name)
+    expect(long_joe).not_to be_valid
+  end
+
+  it 'restricts user.email to 250 characters' do
+    long_email = 'a' * 251 + '@email.com'
+    long_email_joe = FactoryGirl.build(:user, email: long_email)
+    expect(long_email_joe).not_to be_valid
+  end
 end
