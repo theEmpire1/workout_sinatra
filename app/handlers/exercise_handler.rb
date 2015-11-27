b1d486853de5a20d795cc87fa045ae2fec4166be
Drew <drew.e.boardman@gmail.com>
2015-11-27 15:02:30 -0500

    Namespaced ExerciseHandler

--- app/handlers/exercise_handler.rb
+++ app/handlers/exercise_handler.rb
@@ -1,7 +1,9 @@
 class ExerciseHandler < WorkoutBase
-  get '/exercises' do
-    check_authentication
-    exercises = Exercise.all
-    jbuilder :exercises, locals: { exercises: exercises }
+  namespace '/exercise' do
+    get '/all' do
+      check_authentication
+      exercises = Exercise.all
+      jbuilder :exercises, locals: { exercises: exercises }
+    end
   end
 end
--- spec/handlers/exercise_handler_spec.rb
+++ spec/handlers/exercise_handler_spec.rb
@@ -5,12 +5,12 @@ describe WorkoutApp do
     WorkoutApp
   end
 
-  describe '/exercises' do
+  describe '/exercise/all' do
     it 'lists all exercises' do
       user = FactoryGirl.create(:user)
       login_as user
       FactoryGirl.create_list(:exercise, 3)
-      get '/exercises'
+      get '/exercise/all'
       returned = JSON.parse(last_response.body)['exercises']
       expect(last_response.status).to eq(200)
       expect(returned.size).to eq(3)
@@ -18,7 +18,7 @@ describe WorkoutApp do
 
     it 'requires a user to be logged in' do
       FactoryGirl.create(:exercise)
-      get '/exercises'
+      get '/exercise/all'
       expect(last_response.status).to eq(401)
     end
   end
